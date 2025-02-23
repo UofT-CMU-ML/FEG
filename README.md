b03f9f8bc1c8c737f115375fbeb11bbef94a0578
jw954 <junhaowang0601@gmail.com>
2025-02-22 19:55:45 -0500

    FEG Repo Init

---
+++ README.md
@@ -0,0 +1,102 @@
+# EndoGaussian: Gaussian Splatting for Deformable Surgical Scene Reconstruction
+
+## arXiv Preprint
+
+### [Project Page]()| [arXiv Paper]()
+
+
+[Kai Li](https://www.linkedin.com/in/kaii-li/)<sup>1*</sup>, [Junhao Wang](https://www.linkedin.com/in/junhao-wang-4ba488200/)<sup>1*</sup>,
+[William Han](https://willxxy.github.io)<sup>2*</sup>, [Ding Zhao](https://scholar.google.com/citations?user=z7tPc9IAAAAJ&hl=en)<sup>2*</sup>
+
+<sup>1</sup>Department of Engineering Science, University of Toronto; <sup>2</sup>Computer Science Department, Carnegie Mellon University;
+
+<sup>\*</sup> Equal Contributions. <sup>✉</sup> Corresponding Author. 
+
+-------------------------------------------
+
+
+## Commands to run the code
+
+### Get correct CUDA version (If you get error)
+
+```
+export CUDA_HOME=/usr/local/cuda-11.8
+export PATH=$CUDA_HOME/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+```
+
+
+### Environment Setup
+
+```
+git submodule update --init --recursive
+
+conda create -n EndoGaussian python=3.10
+
+conda activate EndoGaussian
+
+pip install -r requirements.txt
+
+pip install -e submodules/depth-diff-gaussian-rasterization
+
+pip install -e submodules/simple-knn
+```
+
+### Get the embeddings
+
+```
+cd encoders/sam_encoder
+pip install -e .
+```
+Download the following same encoders
+
+ViT-H: https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+
+ViT-L: https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
+
+ViT-B: https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
+
+Place it in `encoders/sam_encoder/checkpoints`
+
+```
+cd encoders/sam_encoder
+python export_image_embeddings.py --checkpoint checkpoints/sam_vit_h_4b8939.pth --model-type vit_h --input <dataset_path>/images  --output <data_path>/sam_embeddings
+```
+
+
+### For training:
+
+Use below to try training directly on endonerf data (using feature3dgs train script)
+
+```
+python train.py -s ../pulling_soft_tissues --port 6017 --expname endonerf/pulling --configs arguments/endonerf/pulling.py
+```
+
+
+### Rendering
+
+```
+python render.py --model_path output/endonerf/pulling  --skip_train --skip_video --configs arguments/endonerf/pulling.py
+```
+
+
+### Evaluation
+
+```
+python metrics.py --model_path output/endonerf/pulling
+```
+
+
+expected folder structure 
+
+```
+train 
+ --images
+ --sam_embeddings
+ --sparse/0
+   --cameras.bin
+   --images.bin
+   --points3D.bin
+   --points3D.ply
+   --project.ini
+```
---
+++ scene/deformation.py
@@ -0,0 +1,173 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+from scene.hexplane import HexPlaneField
+
+class Deformation(nn.Module):
+    def __init__(self, D=8, W=256, input_ch=27, input_ch_time=9, skips=[], args=None):
+        super(Deformation, self).__init__()
+        self.D = D
+        self.W = W
+        self.input_ch = input_ch
+        self.input_ch_time = input_ch_time
+        self.skips = skips
+        self.no_grid = args.no_grid
+        
+        self.grid = HexPlaneField(args.bounds, args.kplanes_config, args.multires)
+        self.pos_deform, self.scales_deform, self.rotations_deform, self.opacity_deform, self.semantic_deform = self.create_net()
+        self.args = args
+        
+    def create_net(self):
+        mlp_out_dim = 0
+        if self.no_grid:
+            self.feature_out = [nn.Linear(4,self.W)]
+        else:
+            self.feature_out = [nn.Linear(mlp_out_dim + self.grid.feat_dim ,self.W)]
+        for i in range(self.D-1):
+            self.feature_out.append(nn.ReLU())
+            self.feature_out.append(nn.Linear(self.W,self.W))
+        self.feature_out = nn.Sequential(*self.feature_out)
+
+        sem_feature_dim = 128
+        semantic_deform = nn.Sequential(
+            nn.ReLU(),
+            nn.Linear(self.W, 128),
+            nn.ReLU(),
+            nn.Linear(128, sem_feature_dim)
+        )
+        
+        return  \
+            nn.Sequential(nn.ReLU(),nn.Linear(self.W,self.W),nn.ReLU(),nn.Linear(self.W, 3)),\
+            nn.Sequential(nn.ReLU(),nn.Linear(self.W,self.W),nn.ReLU(),nn.Linear(self.W, 3)),\
+            nn.Sequential(nn.ReLU(),nn.Linear(self.W,self.W),nn.ReLU(),nn.Linear(self.W, 4)), \
+            nn.Sequential(nn.ReLU(),nn.Linear(self.W,self.W),nn.ReLU(),nn.Linear(self.W, 1)), \
+            semantic_deform
+
+    
+    def query_time(self, rays_pts_emb, scales_emb, rotations_emb, time_emb, semantic_feature):
+        if self.no_grid:
+            h = torch.cat([rays_pts_emb[:,:3],time_emb[:,:1]],-1)
+        else:
+            grid_feature = self.grid(rays_pts_emb[:,:3], time_emb[:,:1])
+            h = grid_feature
+        h = self.feature_out(h)
+        return h
+
+    def forward(self, rays_pts_emb, scales_emb=None, rotations_emb=None, opacity = None, time_emb=None, semantic_feature=None):
+        if time_emb is None:
+            return self.forward_static(rays_pts_emb[:,:3])
+        else:
+            return self.forward_dynamic(rays_pts_emb, scales_emb, rotations_emb, opacity, time_emb, semantic_feature)
+
+    def forward_static(self, rays_pts_emb):
+        grid_feature = self.grid(rays_pts_emb[:,:3])
+        dx = self.static_mlp(grid_feature)
+        return rays_pts_emb[:, :3] + dx
+
+    def forward_dynamic(self,rays_pts_emb, scales_emb, rotations_emb, opacity_emb, time_emb, semantic_feature):
+        hidden = self.query_time(rays_pts_emb, scales_emb, rotations_emb, time_emb, semantic_feature).float()
+        
+        if self.args.no_dx:
+            pts = rays_pts_emb[:, :3]
+        else:
+            dx = self.pos_deform(hidden)
+            pts = rays_pts_emb[:, :3] + dx
+        
+        if self.args.no_ds:
+            scales = scales_emb[:,:3]
+        else:
+            ds = self.scales_deform(hidden)
+            scales = scales_emb[:,:3] + ds
+            
+        if self.args.no_dr:
+            rotations = rotations_emb[:,:4]
+        else:
+            dr = self.rotations_deform(hidden)
+            rotations = rotations_emb[:,:4] + dr
+            
+        if self.args.no_do:
+            opacity = opacity_emb[:,:1] 
+        else:
+            do = self.opacity_deform(hidden) 
+            opacity = opacity_emb[:,:1] + do
+
+        use_sem_feat = True
+        # if self.args.no_sf:
+        if use_sem_feat == False:
+            updated_sem_feat = semantic_feature[:,:,:256]
+        else:
+#TODO: implement df, and semantic deform network, and get the resulting sem_feature
+            sem_feature_flat = semantic_feature.squeeze(1)  # => [N,256]
+            df = self.semantic_deform(hidden)                # => [N,256]
+            updated_sem_feat = sem_feature_flat[:,:256] + df 
+            updated_sem_feat = updated_sem_feat.unsqueeze(1)
+
+        return pts, scales, rotations, opacity, updated_sem_feat
+    
+    def get_mlp_parameters(self):
+        parameter_list = []
+        for name, param in self.named_parameters():
+            if  "grid" not in name:
+                parameter_list.append(param)
+        return parameter_list
+    
+    def get_grid_parameters(self):
+        return list(self.grid.parameters()) 
+
+class deform_network(nn.Module):
+    def __init__(self, args) :
+        super(deform_network, self).__init__()
+        net_width = args.net_width
+        timebase_pe = args.timebase_pe
+        defor_depth= args.defor_depth
+        posbase_pe= args.posebase_pe
+        scale_rotation_pe = args.scale_rotation_pe
+        opacity_pe = args.opacity_pe
+        
+        timenet_width = args.timenet_width
+        timenet_output = args.timenet_output
+        times_ch = 2*timebase_pe+1
+        self.timenet = nn.Sequential(
+            nn.Linear(times_ch, timenet_width), nn.ReLU(),
+            nn.Linear(timenet_width, timenet_output))
+        
+        self.deformation_net = Deformation(W=net_width, D=defor_depth, input_ch=(4+3)+((4+3)*scale_rotation_pe)*2, input_ch_time=timenet_output, args=args)
+        
+        self.register_buffer('time_poc', torch.FloatTensor([(2**i) for i in range(timebase_pe)]))
+        self.register_buffer('pos_poc', torch.FloatTensor([(2**i) for i in range(posbase_pe)]))
+        self.register_buffer('rotation_scaling_poc', torch.FloatTensor([(2**i) for i in range(scale_rotation_pe)]))
+        self.register_buffer('opacity_poc', torch.FloatTensor([(2**i) for i in range(opacity_pe)]))
+        self.apply(initialize_weights)
+    
+    def forward(self, point, scales=None, rotations=None, opacity=None, times_sel=None, semantic_feature=None):
+        if times_sel is not None:
+            return self.forward_dynamic(point, scales, rotations, opacity, times_sel, semantic_feature)
+        else:
+            return self.forward_static(point)
+        
+    def forward_static(self, points):
+        points = self.deformation_net(points)
+        return points
+
+    def forward_dynamic(self, point, scales=None, rotations=None, opacity=None, times_sel=None, semantic_feature=None):
+        # times_emb = poc_fre(times_sel, self.time_poc)
+        means3D, scales, rotations, opacity, semantic_feature = self.deformation_net( point,
+                                                scales,
+                                                rotations,
+                                                opacity,
+                                                times_sel, semantic_feature)
+        return means3D, scales, rotations, opacity, semantic_feature
+    
+    def get_mlp_parameters(self):
+        return self.deformation_net.get_mlp_parameters() + list(self.timenet.parameters())
+    
+    def get_grid_parameters(self):
+        return self.deformation_net.get_grid_parameters()
+
+def initialize_weights(m):
+    if isinstance(m, nn.Linear):
+        # init.constant_(m.weight, 0)
+        init.xavier_uniform_(m.weight,gain=1)
+        if m.bias is not None:
+            init.xavier_uniform_(m.weight,gain=1)
+            # init.constant_(m.bias, 0)
